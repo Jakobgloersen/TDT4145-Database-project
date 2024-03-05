53dee049e9db3a94516b48d36a860b0b242d5737
Torbjørn Rolstad <torbjornrolstad@Torbjrn-sin-MacBook-Air.local>
2024-03-05 19:45:56 +0100

    sql-scriptet funker nå
    prøvde å gå igjennom og se over. Rettet opp i det som var feil, sett litt på on delete/update og lagt inn litt kommentarer

--- teater.sql
+++ teater.sql
@@ -11,24 +11,27 @@ drop table Forestilling;
 drop table Kundegruppe;
 drop table Billettkjop;
 drop table Kunde;
+drop table Billett;
+drop table Harkundegruppe;
+drop table Poststeder;
+drop table Rolle;
+drop table Skuespillerirolle;
+drop table Utforesav;
 
 
 
 create table Teater(
-    teaternavn  varchar(40) not null
+    teaternavn  varchar(40) not null,
     constraint teaternavn_pk primary key (teaternavn)
 );
 
-
-CREATE TABLE TeaterSal(
+CREATE TABLE Teatersal(
     teaternavn varchar(40) not null,
     salnavn varchar(40) not null,
-    constraint teaternavn_fk foreign key (teaternavn)
-        references teater(teaternavn)
+    constraint teaternavn_fk foreign key (teaternavn) references Teater(teaternavn)
         on update cascade
-        on delete cascade,
-    constraint teaternavn_salnavn_pk primary key(teaternavn, salnavn)
-    
+        on delete cascade, -- kan ikke ha en teatersal uten et teater, derfor cascade. kan være farlig. Vurdere restrict?
+    constraint teatersal_pk primary key(teaternavn, salnavn)
 );
 
 create table Teaterstykke(
@@ -36,8 +39,8 @@ create table Teaterstykke(
     tittel  varchar(40),
     teaternavn varchar(40) not null,
     salnavn varchar(40) not null,
-    constraint stykkeid_pk primary key (stykkeid),
-    constraint teatersal_fk foreign key (teaternavn, salnavn) references TeaterSal(teaternavn, salnavn)(teaternavn,salnavn)
+    constraint teaterstykke_pk primary key (stykkeid),
+    constraint teatersal_fk foreign key (teaternavn, salnavn) references Teatersal(teaternavn, salnavn)
         on update cascade
         on delete set null
 );
@@ -47,7 +50,7 @@ create table Akt(
     aktnr integer not null,
     navn varchar(40),
     constraint akt_pk primary key (stykkeid, aktnr),
-    constraint akt_fk foreign key (stykkeid) references Teaterstykke(stykkeid)(stykkeid)
+    constraint akt_fk foreign key (stykkeid) references Teaterstykke(stykkeid)
         on update cascade
         on delete set null
 );
@@ -56,47 +59,44 @@ create table Akt(
 create table Person(
     pid integer not null,
     navn varchar(40),
-    ansattstatus varchar(40),
-    constraint pid_pk primary key (pid)
+    constraint person_pk primary key (pid)
 );
 
 create table Ansatt(
     pid integer not null,
-    epost varchar (40),
+    epost varchar(40),
+    ansattstatus varchar(40),
     constraint ansatt_pk primary key (pid),
     constraint ansatt_fk foreign key (pid) references Person(pid)
         on update cascade
-        on delete set null
-);
-
-
-create table Rolle(
-    stykkeid integer not null 
-    aktnr integer not null
-    rollenavn varchar(40) not null
-    constraint rolle_pk primary key (stykkeid, aktnr, rollenavn)
-    constraint akt_fk foreign key (stykkeid, aktnr) references Akt(stykkeid, aktnr)
-        on update cascade
         on delete cascade
-    
-    
 );
 
 create table Skuespiller(
     pid integer not null,
     constraint skuespiller_pk primary key (pid),
-    constraint skuespiller_fk foreign key (pid) references Person(pid)(pid)
+    constraint skuespiller_fk foreign key (pid) references Person(pid)
         on update cascade
-        on delete set null -- kanskje cascade
+        on delete cascade
+);
+
+create table Rolle(
+    stykkeid integer not null ,
+    aktnr integer not null,
+    rollenavn varchar(40) not null,
+    constraint rolle_pk primary key (stykkeid, aktnr, rollenavn),
+    constraint rolle_fk foreign key (stykkeid, aktnr) references Akt(stykkeid, aktnr)
+        on update cascade
+        on delete cascade -- sletter roller hvis en akt blir slettet
 );
 
 create table Oppgave(
     stykkeid integer not null,
-    oppgavenavn varchar(40),
+    oppgavenavn varchar(40) not null,
     constraint oppgave_pk primary key (stykkeid, oppgavenavn),
     constraint oppgave_fk foreign key (stykkeid) references Teaterstykke(stykkeid)
         on update cascade
-        on delete set null
+        on delete set null 
 );
 
 create table Stol(
@@ -106,7 +106,7 @@ create table Stol(
     radnr integer not null,
     omrode varchar(40) not null,
     constraint stol_pk primary key (teaternavn, salnavn, stolnr, radnr, omrode),
-    constraint stol_fk foreign key (teaternavn, salnavn) references TeaterSal(teaternavn,salnavn)
+    constraint stol_fk foreign key (teaternavn, salnavn) references Teatessal(teaternavn,salnavn)
         on update cascade
         on delete set null
 );
@@ -117,15 +117,40 @@ create table Forestilling(
     klokkeslett TIME,
     stykkeid integer not null,
     constraint forestilling_pk primary key (fid),
-    constraint stykkeid_fk foreign key (stykkeid) references Teaterstykke(stykkeid)
+    constraint forestilling_fk foreign key (stykkeid) references Teaterstykke(stykkeid)
         on update cascade
-        on delete set null,
+        on delete set null
 );
 
 create table Kundegruppe(
-    navn varchar(40) not null,
+    navn varchar(40) not null, -- vurdere å endre dette til å hete kundegruppe? ikke så intuitivt
     minimumantall integer,
     constraint kundegruppe_pk primary key (navn)
+);
+
+create table Billett(
+    billettid integer not null,
+    stolnr integer,
+    radnr integer,
+    omrode varchar(40),
+    salnavn varchar(40),
+    teaternavn varchar(40),
+    fid integer not null,
+    kjopid integer not null,
+    kundegruppe varchar(40), 
+    constraint billett_pk primary key (billettid),
+    constraint billett_stol_fk foreign key (stolnr, radnr, omrode, salnavn, teaternavn) references Stol(stolnr, radnr, omrode, salnavn, teaternavn)
+        on update cascade
+        on delete set null,
+    constraint billett_kjop_fk foreign key (kjopid) references Billettkjop(kjopid)
+        on update cascade
+        on delete set null,
+    constraint billett_forestilling_fk foreign key (fid) references Forestilling(fid)
+        on update cascade
+        on delete set null,
+    constraint billett_kundegruppe_fk foreign key (kundegruppe) references Kundegruppe(navn)
+        on update cascade
+        on delete set null
 );
 
 create table Billettkjop(
@@ -133,66 +158,60 @@ create table Billettkjop(
     dato DATE,
     tid TIME, 
     kundeid integer not null,
-    constraint billettkjop primary key (kjopid),
+    constraint billettkjop_pk primary key (kjopid),
     constraint kundeid_fk foreign key (kundeid) references Kunde(kundeid)
         on update cascade
-        on delete set null,
+        on delete set null
 );
 
 create table Kunde(
-    kundeid integer,
-    navn VARCHAR(30) not null,
+    kundeid integer not null,
+    navn varchar(40),
     mobilnummer integer(8),
-    postnummer integer(4) not null,
+    postnr integer(4) not null,
     gatenavn varchar(40),
-    gatenummer integer,
+    gatenummer varchar(10), -- må gjøre mulig med bokstaver
     constraint kunde_pk primary key (kundeid),
-    constraint kunde_fk1 foreign key (postnr) references poststeder(postnr)
+    constraint kunde_fk foreign key (postnr) references Poststeder(postnr)
 		on update cascade
 		on delete set null
-
 );
-create table poststeder(
+
+create table Poststeder(
     postnr 		char(4),
 	poststed 	varchar(30) not null,
 	constraint 	poststeder_pk primary key (postnr) 
-    
 );
 
 -- RELASJONSKLASSER --
 
-CREATE TABLE UtforesAv (
-    stykkeid INTEGER NOT NULL,
-    oppgavenavn VARCHAR(40) NOT NULL,
-    pid INTEGER NOT NULL,
-    
-    CONSTRAINT utforesav_pk PRIMARY KEY (stykkeid, oppgavenavn, pid),
-    CONSTRAINT fk_UtforesAv_oppgavenavn FOREIGN KEY (stykkeid, oppgavenavn) REFERENCES Oppgave(stykkeid, oppgavenavn) 
+create table Utforesav (
+    stykkeid integer not null,
+    oppgavenavn varchar(40) not null,
+    pid integer not null,
+    constraint utforesav_pk primary key (stykkeid, oppgavenavn, pid),
+    constraint utforesav_oppgave_fk foreign key (stykkeid, oppgavenavn) references Oppgave(stykkeid, oppgavenavn) 
         on update cascade
         on delete set null,
-    CONSTRAINT fk_UtforesAv_Pid FOREIGN KEY (pid) REFERENCES Ansatt(pid)
+    constraint utforesav_pid_fk foreign key (pid) references Ansatt(pid)
         on update cascade
-        on delete set null,
+        on delete set null
 );
 
-
-CREATE TABLE HarKundeGruppe (
-    kundegruppe INTEGER NOT NULL,
-    stykkeID INTEGER NOT NULL,
-    pris DECIMAL(10, 2) NOT NULL,
-    
-    CONSTRAINT pk_HarKundeGruppe PRIMARY KEY (Kundegruppe, StykkeID),
-    CONSTRAINT fk_HarKundeGruppe_Kundegruppe FOREIGN KEY (Kundegruppe) REFERENCES Kundegruppe(Kundegruppe)
+create table Harkundegruppe (
+    kundegruppe varchar(40) not null, -- greit at dette bare heter kundegruppe, og ikke har samme navn som i kundegruppe?
+    stykkeid integer not null,
+    pris decimal(10, 2) not null,
+    constraint harkundegruppe_pk primary key (kundegruppe, stykkeid),
+    constraint har_kundegruppe_fk foreign key (kundegruppe) references Kundegruppe(navn)
         on update cascade
         on delete set null,
-    CONSTRAINT fk_HarKundeGruppe_StykkeID FOREIGN KEY (StykkeID) REFERENCES TeaterStykke(StykkeID)
+    constraint stykke_har_kundegruppe_fk foreign key (stykkeid) references Teaterstykke(stykkeid)
         on update cascade
-        on delete set null,
+        on delete set null
 );
 
-
-
-create table SkuespillerIRolle(
+create table Skuespillerirolle(
     pid integer not null,
     stykkeid integer not null,
     aktnr integer not null,
@@ -201,7 +220,7 @@ create table SkuespillerIRolle(
     constraint rolle_fk foreign key (stykkeid, aktnr, rollenavn) references Rolle(stykkeid, aktnr, rollenavn)
         on update cascade
         on delete set null,
-    constraint pid_fk foreign key references Skuespiller(pid)
+    constraint pid_i_rolle_fk foreign key (pid) references Skuespiller(pid)
         on update cascade
         on delete set null
 );
