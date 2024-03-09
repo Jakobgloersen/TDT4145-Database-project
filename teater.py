0ef36b80c0871ef1066761a9088486a0a5e0218f
Torbjørn Rolstad <torbjornrolstad@Torbjrn-sin-MacBook-Air.local>
2024-03-09 21:09:18 +0100

    brukerhistorie 4

--- teater.py
+++ teater.py
@@ -107,5 +107,37 @@ def create_db():
     con.commit()
     con.close()
 
+def brukerhistorie4(dato):
+    con = sqlite3.connect("teater.db")
+    cursor = con.cursor()
+    # henter ut forestillinger på gitt dato
+    cursor.execute('''select fid, tittel from Forestilling join Teaterstykke using(stykkeid) where dato = ?''', (str(dato),))
+    plays = cursor.fetchall()
+    # sjekker om spørringen gir et svar
+    if not plays:
+        print("\n\nDet er ingen forestillinger på denne datoen.\n\n")
+        return
+    # dersom spørringen ikke er tom
+    print(f"\n\nForestillinger på {dato}:\n")
+    for play in plays:
+        # finner solgte billetter for hver forestilling på gitt datp
+        cursor.execute('''select count(billettid) from Billett where fid = ?''', (play[0],))
+        sold = cursor.fetchone()[0]
+        print(f"Forestilling: '{play[1]}'. Solgte billetter: {sold}")
+    print("\n")
+
 if __name__=="__main__":
     create_db()
+    # loop som tillater bruker å velge brukerhistorie de ønsker å gjøre
+    while(1):
+        print("Brukerhistorie 4 - skriver ut forestilling og antall solgte billetter per forestilling for en gitt dato")
+        print("Avslutt - skriv 0")
+        inp = input("Skriv brukerhistorie du ønsker å utføre: ")
+        if inp == '0':
+            exit()
+        elif inp == '4':
+            dato = input("Skriv inn dato du ønsker å se forestillinger for på formatet yyyy-mm-dd: ") # input burde valideres
+            brukerhistorie4(dato)
+        else:
+            print("\nOppgi brukerhistorien som et tall som representerer en av de gitte brukerhistoriene, for eksempel '4'. For å avslutte, skriv inn '0'.\n")
+        
