f5240e91ce0c3ecf1bf4f835fc99ed4ed7cc1365
Jakob Gloersen <jakobmgl@stud.ntnu.no>
2024-03-11 15:53:46 +0100

    fix billettkjøp salvnavn bug

--- teater.py
+++ teater.py
@@ -225,16 +225,20 @@ def brukerhistorie3():
 
     # Henter nåværende dato og tid
     naa = datetime.datetime.now()
-    dato = naa.strftime('%Y-%m-%d')
-    tidspunkt = naa.strftime('%H:%M:%S')
-    
+    dato = naa.strftime("%Y-%m-%d")
+    tidspunkt = naa.strftime("%H:%M:%S")
+
     # finner den nåværende høyeste kjøpid og legge til 1 for det nye kjøpet
-    cursor.execute(''' select max(kjopid) FROM Billettkjop''')
+    cursor.execute(""" select max(kjopid) FROM Billettkjop""")
     max_id = cursor.fetchone()[0]
-    ny_kjopid = max_id + 1 if max_id is not None else 1 #Starter på 1 hvis tabellen er tom
+    ny_kjopid = (
+        max_id + 1 if max_id is not None else 1
+    )  # Starter på 1 hvis tabellen er tom
 
     # opprette et kjøp for de ni billettene med default bruker og bruker ny_kjopid for kjøpet
-    cursor.execute('''insert into Billettkjop values (?, ?, ?, 0)''', (ny_kjopid, dato, tidspunkt))
+    cursor.execute(
+        """insert into Billettkjop values (?, ?, ?, 0)""", (ny_kjopid, dato, tidspunkt)
+    )
     # finner fid
     cursor.execute(
         """select fid from Forestilling where dato = '2024-02-03' and stykkeid = 1"""
@@ -265,12 +269,13 @@ def brukerhistorie3():
     cursor.execute(
         """select stolnr
                         from Stol 
-                        where Stol.omrode = ? and Stol.radnr = ? and not exists(
+                        where Stol.salnavn = 'Gamle scene' and Stol.omrode = ? and Stol.radnr = ? and not exists(
                             select stolnr
                             from Billett
                             where fid = ?
                                 and Billett.stolnr = Stol.stolnr
                                 and Billett.radnr = Stol.radnr
+                                and Billett.salnavn = Stol.salnavn
                                 and Billett.omrode = Stol.omrode)""",
         (
             rad[1],
@@ -283,14 +288,19 @@ def brukerhistorie3():
         print("\n\nFant ikke ni billetter på samme rad.\n\n")
         return
     print("\n\nKjøpte billetter:\n\n")
-    #billettid = 1000 # tilfeldig valgt for å ha unik id
+
     # finner den nåværende høyeste billettid og legg til 1 for den første nye billetten
-    cursor.execute('''select max(billettid) from Billett''')
+    cursor.execute("""select max(billettid) from Billett""")
     max_billettid = cursor.fetchone()[0]
-    ny_billettid = max_billettid + 1 if max_billettid is not None else 0 # Starter på 1000 hvis tabellen er tom
+    ny_billettid = max_billettid + 1 if max_billettid is not None else 0
     for billett in billetter:
-        cursor.execute('''insert into Billett values (?, ?, ?, ?, 'Gamle scene', 'Trøndelag Teater', ?, ?, 'Ordinær')''', (ny_billettid, billett[0], rad[0], rad[1], fid, ny_kjopid)) # antar ordinære billetter
-        print(f"Billett-id: {ny_billettid}. Stol: {billett[0]} Rad: {rad[0]}. Område: {rad[1]}")
+        cursor.execute(
+            """insert into Billett values (?, ?, ?, ?, 'Gamle scene', 'Trøndelag Teater', ?, ?, 'Ordinær')""",
+            (ny_billettid, billett[0], rad[0], rad[1], fid, ny_kjopid),
+        )  # antar ordinære billetter
+        print(
+            f"Billett-id: {ny_billettid}. Stol: {billett[0]} Rad: {rad[0]}. Område: {rad[1]}"
+        )
         ny_billettid += 1
     con.commit()
     con.close()
