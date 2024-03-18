66a76d14487cf08d056fbc759b1864359c2da340
Jakob Gloersen <jakobmgl@stud.ntnu.no>
2024-03-18 09:10:10 +0100

    refactor brukerhistorie 1 to own file

---
+++ brukerhistorie1.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+# lager db fil hvis ikke allerede eksisterer
+f = open("teater.db", "w")
+# lager tabeller fra teater.sql
+with open("teater.sql", "r") as create_sql:
+    create_script = create_sql.read()
+con = sqlite3.connect("teater.db")
+cursor = con.cursor()
+cursor.executescript(create_script)
+# leser inn sql script og setter inn i tabeller
+with open("insert_teater.sql", "r") as insert_sql:
+    insert_script = insert_sql.read()
+cursor.executescript(insert_script)
+
+with open("postnummer.txt", "r", encoding="utf-16") as f:
+    postnummer = f.readlines()
+for linje in postnummer:
+    deler = linje.strip().split("\t")
+    postnr1 = deler[0]
+    poststed1 = deler[1]
+
+    cursor.execute(
+        "INSERT INTO Poststeder (postnr, poststed) VALUES (?, ?)",
+        (postnr1, poststed1),
+    )
+
+con.commit()
+con.close()
